4cfb847961690c7edbd344c36eb24e2203a22507
yovelrod <118877588+yovelrod@users.noreply.github.com>
2022-12-09 16:25:44 +0200

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -137,8 +137,8 @@ st.sidebar.text('Original Image')
 st.sidebar.image(image)
 
 # call the function to segment the image
-
+segmented_image = otsu_segment(image, sigma=sigma_value)
 
 # Display the result on the right (main frame)
 st.subheader('Output Image')
-
+st.image(segmented_image, use_column_width=True)
